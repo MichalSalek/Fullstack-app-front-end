f70bcc7e576bd4476cc674b69d9ba2b106b73a55
Michał Sałek <michal.salek@abventor.com>
2019-11-17 12:20:53 -0500

    Scroll to top component added to app

---
+++ src/components/scroll-to-top.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+// Material
+import useScrollTrigger from "@material-ui/core/useScrollTrigger/useScrollTrigger";
+import Zoom from "@material-ui/core/Zoom";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+}));
+
+export const ScrollTop = (props) => {
+    const classes = useStyles();
+
+    const trigger = useScrollTrigger({
+        target: window,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = event => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
+    };
+
+    return (
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation" className={classes.root}>
+                {props.children}
+            </div>
+        </Zoom>
+    );
+};
