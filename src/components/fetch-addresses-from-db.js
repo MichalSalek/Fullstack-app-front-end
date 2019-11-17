bb2469be458cbcc50ec0c5682e8ff1a90c20184b
Michał Sałek <michal.salek@abventor.com>
2019-11-17 12:20:00 -0500

    Axios added

---
+++ src/components/fetch-addresses-from-db.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Material
+import Paper from "@material-ui/core/Paper";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Box from "@material-ui/core/Box";
+
+export const AddAddressToDB = () => {
+    return (<React.Fragment>
+        <Paper><Box px={1} py={3}> sdf </Box></Paper>
+    </React.Fragment>)
+};
