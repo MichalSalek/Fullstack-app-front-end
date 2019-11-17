fc32dcedaefde9a8d8eb44a66b6250d3887b06eb
Michał Sałek <michal.salek@abventor.com>
2019-11-17 15:37:55 -0500

    Better logic for scroll to top component

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -22,12 +22,8 @@ export const ScrollTop = (props) => {
         threshold: 100,
     });
 
-    const handleClick = event => {
-        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-
-        if (anchor) {
-            anchor.scrollIntoView({behavior: 'smooth', block: 'center'});
-        }
+    const handleClick = () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
     };
 
     return (
