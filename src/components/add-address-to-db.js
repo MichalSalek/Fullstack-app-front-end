e97c9440d156658923293ea019c759dcc163e08e
Michał Sałek <michal.salek@abventor.com>
2019-11-17 13:31:13 -0500

    Added Pop-up infos

--- src/components/add-address-to-db.js
+++ src/components/add-address-to-db.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import service$ from '../common/services/http.service';
+import {SnackbarWrapper} from "./snackbar-wrapper";
 
 // Material
 import Paper from "@material-ui/core/Paper";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
 import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -22,32 +20,53 @@ const useStyles = makeStyles(theme => ({
     },
     leftSpacing: {
         marginLeft: "0.5rem",
-    },
-    leftSpacingx2: {
-        marginLeft: "2rem",
     }
 }));
 
 export const AddAddressToDB = () => {
     const classes = useStyles();
+    const [address, setAddress] = useState("");
+    const [openSnackbar, setOpenSnackbar] = React.useState(false);
+    const [sendCounter, setSendCounter] = React.useState(0);
+
+    const sendAddressToDB = () => {
+        service$.sendAddress(address).then((response) => {
+            if (response.status === 200) {
+                setSendCounter(sendCounter + 1);
+                setAddress("");
+                setOpenSnackbar(true);
+
+            }
+        })
+    };
+
     return (<React.Fragment>
         <Paper><Box px={2} py={3}>
             <Typography variant={"h5"} component={"h3"} align={"center"}>Add new address</Typography>
             <article className={classes.root}>
                 <MonetizationOnOutlinedIcon/>
                 <TextField className={classes.leftSpacing} id="add-address" label="Add new bitcoin address here"
-                           fullWidth/>
+                           fullWidth
+                           onChange={(e) => setAddress(e.currentTarget.value)}
+                           value={address}/>
                 <Button
-                    className={classes.leftSpacingx2}
-                    inverted
-                    variant="contained"
+                    className={classes.leftSpacing}
+                    variant="outlined"
                     color="primary"
                     size="small"
                     startIcon={<SaveIcon/>}
+                    onClick={sendAddressToDB}
                 >
                     Save
                 </Button>
             </article>
         </Box></Paper>
+        <SnackbarWrapper
+            variant="success"
+            className={classes.margin}
+            message={`The new address (${sendCounter}) added successfully!`}
+            isItOpen={openSnackbar}
+            setIsItOpen={setOpenSnackbar}
+        />;
     </React.Fragment>)
 };
